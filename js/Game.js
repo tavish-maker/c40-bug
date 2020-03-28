fcfd7c8ef8af4ad1d03ea83b62951dde69736804
tavish-maker <tavishsaluja05@gmail.com>
2020-03-28 10:52:52 +0530

    bug

--- js/Game.js
+++ js/Game.js
@@ -44,6 +44,7 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
+    player.getCarsAtEnd();
     
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
@@ -91,12 +92,19 @@ class Game {
 
     if(player.distance > 3860){
       gameState = 2;
+      player.rank+=1
+      Player.updateCarsAtEnd(player.rank);
     }
+
+
+
+
    
     drawSprites();
   }
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank)
   }
 }
